20475e207ae2f644574c24b1cc533e81ff98cd41
Lini Abraham <liniabraham007@gmail.com>
2025-08-03 19:20:34 +1000

    added code for angular standalone project

---
+++ src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,34 @@
+import { Component, computed, inject, input } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { InvestmentService } from '../investment.service';
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: true,
+  imports: [CurrencyPipe],
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css',
+})
+export class InvestmentResultsComponent {
+  // results = input<
+  //   {
+  //     year: number;
+  //     interest: number;
+  //     valueEndOfYear: number;
+  //     annualInvestment: number;
+  //     totalInterest: number;
+  //     totalAmountInvested: number;
+  //   }[]
+  // >();
+
+  private investmentService = inject(InvestmentService);
+
+  // get results() {
+  //   return this.investmentService.resultsData;
+  // }
+
+  results = computed(() => this.investmentService.resultsData());
+  // or
+
+  // results = this.investmentService.resultsData.asReadonly();
+}
